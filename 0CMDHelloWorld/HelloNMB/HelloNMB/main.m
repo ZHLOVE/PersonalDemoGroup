80dbd076b2844a83d69eb1d87b68dc82f8eeef73
马千里 <maqianli@ysten.com>
2017-08-26 19:35:41 +0800

    C语言写对数

--- 0CMDHelloWorld/HelloNMB/HelloNMB/main.m
+++ 0CMDHelloWorld/HelloNMB/HelloNMB/main.m
@@ -8,11 +8,16 @@
 
 #import <Foundation/Foundation.h>
 #import "HWWeakTimer.h"
-
+#include <math.h>
+#include <stdio.h>
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
        
+        double result;
+        double x = 80;
+        result = pow(x, 2) * 10;
+        printf("The common log of %lf is %lf\n", x, result);
     }
     return 0;
 }
